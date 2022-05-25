5e4186dee637bc6d39af6b3221b169a2344eade2
safal-del <carkeysafal21@gmail.com>
2022-05-25 11:38:44 +0545

    react

---
+++ src/Component/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "../logo192.png";
+
+
+export default function Navbar(){
+
+    return (
+        <nav>
+        <div>
+            <img src={Logo} width = "40px"/>
+            <h1> React Facts</h1>
+
+
+
+
+        </div>
+        </nav>
+    )
+
+}
